0e7cbc4013ad8e943cbdbf58fef32b806aec2036
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-07 16:23:36 +0530

    Create DataType.js

---
+++ DataType.js
@@ -0,0 +1,16 @@
+let num=3;//integer
+let num2=33.33;//float
+let num3=3;//infinity
+let num4='narendra';
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num);
+let str="narendra reddy";
+let str2="welcome";
+console.log(str);
+console.log(str2);
+let str3="reddy";
+let str4="33";
+console.log(str3);
+console.log(str4);
