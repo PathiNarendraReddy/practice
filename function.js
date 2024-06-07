e5e8f6ebbe61f57950cffdc80ada858ed55193dc
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-07 12:17:07 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,18 @@
+var x=55;
+function add(){
+    var x=60;
+    console.log(x);
+    
+     var y=60;
+    console.log(y);
+      console.log(x+y);
+    
+     var z=60;
+    console.log(z);
+        console.log(x+z);
+            console.log(y+z);
+}
+add();
+console.log(x);
+console.log(y);
+console.log(z);
