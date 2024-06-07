d844155bd8592000adc9ef7cbc5f82fa4dabafb0
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-07 12:18:33 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,29 @@
+var x=30;
+var z=90;
+var y=120;
+
+let b=300;
+let c=450;
+function  add(){
+   console.log(x);
+var y=60;
+console.log(y);
+
+let a=250;
+console.log(a);
+}
+add();
+console.log(x+60);
+console.log(z);
+console.log(z+60);
+console.log(y+60);
+
+console.log(b);
+console.log(x+b);
+console.log(y+b);
+console.log(z+b);
+
+console.log(c);
+console.log(x+c);
+console.log(y+c);
+console.log(z+c);
