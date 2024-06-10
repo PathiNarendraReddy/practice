4fad2b47834568f4f23434c539f1ca2a9009adad
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-10 11:20:47 +0530

    Create logical(comperition).js

---
+++ logical(comperition).js
@@ -0,0 +1,20 @@
+var a=13;
+var b=15;
+console.log(a&&b);
+console.log(a||b);
+
+var a1=13;
+var b1=15;
+console.log(a1&&b1);
+console.log(a1||b1);
+
+
+let x=46;
+let y=38;
+console.log(x==y);
+console.log(x!=y);
+console.log(x!==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
