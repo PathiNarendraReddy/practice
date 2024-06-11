bcd0978491b3c5821bff885e0f5759dd3cf29aac
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-11 18:13:23 +0530

    Create searchstring.js

---
+++ searchstring.js
@@ -0,0 +1,6 @@
+let text="hello narendra welcome";
+console.log(text.search("a"));
+console.log(text.search("w"));
+console.log(text.search("e"));
+console.log(text.search("o"));
+console.log(text.search("m"));
