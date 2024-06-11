969b0bd2af08c7400f83bcc81204d880e0ef40c3
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-11 18:07:04 +0530

    Create textlength.js

---
+++ textlength.js
@@ -0,0 +1,15 @@
+let text="Hello narendra reddy";
+console.log(text.search("n"));
+console.log(text.search("a"));
+console.log(text.search("r"));
+console.log(text.search("e"));
+console.log(text.search("n"));
+console.log(text.search("d"));
+console.log(text.search("r"));
+console.log(text.search("a"));
+console.log(text.search("r"));
+console.log(text.search("e"));
+console.log(text.search("d"));
+console.log(text.search("d"));
+console.log(text.search("y"));
+
