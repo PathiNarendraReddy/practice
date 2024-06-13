4b12aff1f6ccd2f796de815d1d9094979d37f051
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-13 12:58:14 +0530

    Create numerical string.js

---
+++ numerical string.js
@@ -0,0 +1,22 @@
+let x=33;
+console.log(x);
+
+let x1="33"*"333";
+console.log(x1);
+
+let y="66"/"333";
+console.log(y);
+
+let y1="33"-"333";
+console.log(y1);
+
+
+let z="15"%"100";
+console.log(z);
+
+let z1="3366"+"3737";
+console.log(z1);
+
+
+
+
