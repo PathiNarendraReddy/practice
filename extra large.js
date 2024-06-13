495459883aa5b0c8aa27eae19e6d2e943ed29b49
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-13 12:59:19 +0530

    Create extra large.js

---
+++ extra large.js
@@ -0,0 +1,21 @@
+let l=123e4;
+let m=567e-8;
+console.log(l);
+console.log(m);
+
+let c=333333;
+let d=4444444;
+console.log(c);
+console.log(d);
+
+let a=12.33+33.44;
+console.log(a);
+
+let b=12.33-33.44;
+console.log(b);
+
+let a1=12.33/33.44;
+console.log(a1);
+
+let b1=12.33%33.44;
+console.log(b1);
