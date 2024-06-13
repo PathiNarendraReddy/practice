0622c17397942e7b61a10a7c1254f37edd2ae734
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-13 15:43:11 +0530

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,17 @@
+let x=new Date (2001,3,28,11,25,5);
+document.write(x);
+
+let date=new Date();
+let year=date.getFullYear();
+let month=date.getMonth();
+let Day=date.getDay();
+let hours=date.getHours();
+let minutes=date.getMinutes();
+let seconds=date.getSeconds();
+document.write(date);
+document.write(year);
+document.write(month);
+document.write(Day);
+document.write(hours);
+document.write(minutes);
+document.write(seconds);
