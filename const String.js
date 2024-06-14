cef0efc88186dd9ae2754bbcb98b45985ad63887
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-14 12:03:58 +0530

    Create const String.js

---
+++ const String.js
@@ -0,0 +1,11 @@
+const str="narendra Reddy";
+const str1="Pathi"
+console.log(str);
+console.log(str1);
+
+let array=[];//empty array
+const str2="";//empty string
+console.log(str2);
+
+let str3="HI NARENDRA HOW ARE YOU";
+console.log(str3);
