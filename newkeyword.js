6a4292988aa9a04b518681411aa1764c8393508b
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-14 16:30:42 +0530

    Create newkeyword.js

---
+++ newkeyword.js
@@ -0,0 +1,7 @@
+var stu=new Object();
+stu.id=9652;
+stu.name="Narendra Reddy";
+stu.branch="mpcs";
+stu.add="macherla";
+stu.MobileNo=965238;
+document.write(stu.id+""+stu.branch+""+stu.add+""+stu.MobileNo);
