e6eb3a6e412b636ab9a16d9a49c168efe634df9e
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-18 17:03:34 +0530

    Create onclick.js

---
+++ onclick.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title>event example</title>
+        <script src="basic.js"></script>
+    </head>
+    <body>
+    <h1> onclick event example</h1>
+   <button  type="button" onclick="welcome()">click ok.</button>
+   <script>
+    function Narendra(){
+        alert("HELLO NARENDRA REDDY");
+    }
+   </script>
+    </body>
+</html>
