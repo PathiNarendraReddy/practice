1b3bae1c382c386d9c6caaabbff778a72b07e7b7
PathiNarendraReddy <narendrareddyp9652@gmail.com>
2024-06-19 17:01:25 +0530

    Create on blur.js

---
+++ on blur.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+
+        <title>event example</title>
+        <script src="basic.js"></script>
+    </head>
+    <body>
+      
+    <h1> onblur  event example</h1>email:
+  <input type="email" id="email" onblur="narendra()">
+  <script>
+    function narendra(){
+        alert("field is mandetory");
+    }
+  </script>
+ 
+    </body>
+</html>
+
+
+
+
